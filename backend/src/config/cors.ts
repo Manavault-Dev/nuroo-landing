175ee14e17ba1e6be6d48d70c215439b53148925
tilekdzhenishev <tilek.dzenisev@gmail.com>
2026-02-10 21:55:36 +0100

    fix: fixed cors errors and remove auth checkoing for endpoints

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -6,7 +6,12 @@ export function getCorsConfig(config: Config): FastifyCorsOptions {
     origin:
       config.NODE_ENV === 'production'
         ? ['https://usenuroo.com']
-        : ['http://localhost:3000', 'http://localhost:3001'],
+        : [
+            'http://localhost:3000',
+            'http://localhost:3001',
+            'http://127.0.0.1:3000',
+            'http://127.0.0.1:3001',
+          ],
     credentials: true,
   }
 }
